c517066ffa2551db588bb03d35e6feb32568a742
rootTiket <l20078829@gmail.com>
2025-03-26 21:21:11 +0900

    init

---
+++ src/main/java/com/example/muk_jji_ppa/MukJjiPpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.muk_jji_ppa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MukJjiPpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MukJjiPpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/muk_jji_ppa/MukJjiPpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muk_jji_ppa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MukJjiPpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
