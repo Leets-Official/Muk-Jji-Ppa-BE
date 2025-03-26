a6613ce3ac9923c2793f3f75891f25e89e4d6d6f
rootTiket <l20078829@gmail.com>
2025-03-26 21:24:18 +0900

    feat: 컨트롤러 추가

---
+++ src/main/java/com/example/muk_jji_ppa/controller/MukJjiPpaController.java
@@ -0,0 +1,13 @@
+package com.example.muk_jji_ppa.controller;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MukJjiPpaController implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) {
+        //TODO: 구현
+    }
+}
